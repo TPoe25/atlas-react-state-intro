e8169126fc402e939c7c0c45457e93ed59044bd6
Taylor Poe <t_poe25@yahoo.com>
2026-01-09 14:58:03 -0600

    fixed duplicates but need to figure how to include other search

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,6 @@
 import SchoolCatalog from "./SchoolCatalog";
 import Header from "./Header";
 import ClassSchedule from "./ClassSchedule";
-import SetTable from "./components/Sch-cat-fetch";
 import SearchField from "./components/search-field";
 
 export default function App() {
@@ -9,7 +8,6 @@ export default function App() {
     <div>
       <Header />
       <SearchField />
-      <SetTable />
       <SchoolCatalog />
       <ClassSchedule />
     </div>
